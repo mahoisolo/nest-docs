0c081dffece227f28e8b7267b48640346c7e51c1
mahoisolo <133096748+mahoisolo@users.noreply.github.com>
2024-08-11 23:09:23 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
   const port = configService.get<number>('PORT') || 3000;
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(port);
 }
 bootstrap();
